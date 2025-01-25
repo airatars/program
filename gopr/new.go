0a523fb365d0baecb51cbae9913c357c98dfb82c
Airut <airatars05@mail.ru>
2025-01-25 23:03:53 +0300

    hello on GoLang

--- gopr/new.go
+++ gopr/new.go
@@ -4,17 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello")
-
-	//var i = 10
-	for i := 0; i <= 5; i++ {
-		fmt.Println(i)
-	}
-
-	nums := [3]int{5, 1, 9}
-	for i, value := range nums {
-		fmt.Println(value, i)
-	}
 }
-
-// Lorem ipsum dolor sit amet, consectet
-// lorem ipsum dolor sit amet, consectet
